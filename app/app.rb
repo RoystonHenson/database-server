4f61ceec81acb59e161367a4cfc150787a33887a
Royston Henson <rlqhenson@hotmail.com>
2016-11-28 15:25:57 +0000

    added save page

--- app/app.rb
+++ app/app.rb
@@ -10,6 +10,14 @@ class DatabaseServer < Sinatra::Base
     erb :enter_key
   end
 
+  post '/saved' do
+    redirect '/saved'
+  end
+
+  get '/saved' do
+    erb :saved
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
